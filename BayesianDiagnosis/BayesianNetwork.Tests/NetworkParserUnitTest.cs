bdd61b932ffcf9c2a5de47e352d4d29cee6f99b4
Paul-Cristian <brinzapaul@gmail.com>
2019-01-14 02:45:51 +0200

    Network parser seems good at console output, but failes tests as library

---
+++ BayesianDiagnosis/BayesianNetwork.Tests/NetworkParserUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NetworkParser;
+using BayesianNetwork;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BayesianNetwork.Tests
+{
+    [TestClass]
+    public class NetworkParserUnitTest
+    {
+        static NetworkCreate create = new NetworkCreate();
+        static Network mockNetwork = create.startParsing();
+
+        [TestMethod]
+        public void TestAskingSimpleQuestion()
+        {
+            var gripa = mockNetwork.getNodeByName("gripa");
+            var q = new Query(gripa, "False", Enumerable.Empty<Fact>());
+            Assert.AreEqual(0.9, gripa.ProbabilityDistribution[q], 1e-3);
+
+            var o = new Query(gripa, "True", Enumerable.Empty<Fact>());
+            Assert.AreEqual(0.1, gripa.ProbabilityDistribution[o], 1e-3);
+        }
+    }
+}
---
+++ BayesianDiagnosis/NetworkParser/NetworkCreate.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using BayesianNetwork;
+
+
+namespace NetworkParser
+{
+    public class NetworkCreate
+    {
+        static Network network;
+
+        public Network startParsing()
+        {
+            XmlDocument doc = new XmlDocument();
+            network = new Network();
+
+            if (File.Exists("C:/Users/Paul Cristian/Desktop/bayes-net/BayesianDiagnosis/NetworkParser/curs.xml"))
+                doc.Load("C:/Users/Paul Cristian/Desktop/bayes-net/BayesianDiagnosis/NetworkParser/curs.xml");
+            else
+                return network;
+
+            XmlNodeList nodeList = doc.DocumentElement.SelectNodes("/net/node");
+
+            foreach (XmlNode node in nodeList)
+            {
+                string name = node.SelectSingleNode("name").InnerText;
+                network.addNode(name);
+
+                XmlNodeList probabiliyList = doc.DocumentElement.SelectNodes("/net/node/table/probability");
+                foreach (XmlNode p in probabiliyList)
+                {
+                    if (p.ParentNode.ParentNode == node)
+                    {
+                        var given = p.SelectSingleNode("given");
+
+                        if (given.ChildNodes.Count != 0)
+                        {
+                            XmlNodeList factsList = given.SelectNodes("fact");
+                            IDictionary<List<Fact>, string> allFacts = new Dictionary<List<Fact>, string>();
+                            List<Fact> list = new List<Fact>();
+
+                            string externalOutcome = p.SelectSingleNode("outcome").InnerText;
+                            double prob = Double.Parse(p.SelectSingleNode("value").InnerText);
+
+                            //Console.WriteLine(externalOutcome + "->" + prob);
+
+                            foreach (XmlNode f in factsList)
+                            {
+                                string parent = f.SelectSingleNode("parent").InnerText;
+                                string internalOutcome = f.SelectSingleNode("outcome").InnerText;
+
+                                list.Add(new Fact(network.getNodeByName(parent), internalOutcome));
+
+                                //Console.WriteLine(parent + "->" + name);
+                                network.addLink(parent, name);
+                            }
+                            allFacts.Add(list, name);
+                            foreach (KeyValuePair<List<Fact>, string> kvp in allFacts)
+                            {
+                                if (kvp.Value == name)
+                                {
+                                    var disease = network.getNodeByName(name);
+                                    disease.addProbability(new Query(disease, externalOutcome, kvp.Key), prob);
+                                }
+                                //Console.WriteLine("Key = {0}, Value = {1}",
+                                //    kvp.Key, kvp.Value);
+                                //Console.WriteLine(System.Environment.NewLine + externalOutcome);
+                                //Console.WriteLine(System.Environment.NewLine + prob);
+                            }
+                        }
+                        else
+                        {
+                            var disease = network.getNodeByName(name);
+                            string outcome = p.SelectSingleNode("outcome").InnerText;
+                            double prob = Double.Parse(p.SelectSingleNode("value").InnerText);
+                            Console.WriteLine(disease + " " + outcome + " " + prob);
+                            disease.addProbability(new Query(disease, outcome, Enumerable.Empty<Fact>()), prob);
+                        }
+                    }
+                }
+            }
+            return network;
+        }
+    }
+}
