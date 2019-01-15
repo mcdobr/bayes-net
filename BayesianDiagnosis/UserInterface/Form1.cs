88c3a4062e07db4210cd09de63c7d5c9a7d92d80
CIulius <covas_andrei@yahoo.com>
2019-01-15 14:26:50 +0200

    Interfata

--- BayesianDiagnosis/UserInterface/Form1.cs
+++ BayesianDiagnosis/UserInterface/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NetworkParser;
+using BayesianNetwork;
 
 namespace UserInterface
 {
@@ -15,6 +17,65 @@ namespace UserInterface
         public Form1()
         {
             InitializeComponent();
+        }
+        static NetworkCreate create = new NetworkCreate();
+        static Network mockNetwork = create.startParsing();
+
+
+        void sortare(List<string> list)
+        {
+            list.Sort();
+            list.Reverse();
+        }
+        void afisare(List<string> list)
+        {
+            foreach (string i in list)
+            {
+                Afisare.Items.Add(i);
+            }
+        }
+        public void TestAskingInferentialQuestion(List<string> list)
+        {
+            List<Fact> evidence = new List<Fact>();
+            int k = 0;
+            foreach (string i in list)
+            {
+                evidence.Add(new Fact(list[k].Split('|')[0], list[k].Split('|')[1], mockNetwork));
+                k++;
+            }
+            foreach (string i in checkedListBox3.CheckedItems)
+            {
+                evidence.Add( new Fact(i, "true", mockNetwork));
+            }
+            List<string> list_node = new List<string> {"cold","lung cancer","flu","pneumonia","astm" };
+            
+            List<string> list_answer = new List<string>();
+            foreach (string i in list_node)
+            {
+                Node disease = mockNetwork.Nodes[i];
+                Query question = new Query(disease, "true", evidence);
+                list_answer.Add(string.Format("{0} => {1}", mockNetwork.answer(question), disease.ToString()));
+            }
+            sortare(list_answer);
+            afisare(list_answer);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Afisare.Items.Clear();
+            List<CheckedListBox> checkbox_list = new List<CheckedListBox> { checkedListBox1, checkedListBox2, checkedListBox4 };
+            string[] a={ label1.Text, label2.Text, label3.Text };
+            List<string> list = new List<string>();
+            int k = 0;
+            foreach (CheckedListBox s in checkbox_list)
+            {
+                foreach (string i in s.CheckedItems)
+                {
+                    list.Add(string.Format("{0}|{1}",a[k],i));
+                }
+                k++;
+            }
+            afisare(list);
+            TestAskingInferentialQuestion(list);
         }
     }
 }
